a6f53913e0c9c70f9749518f9d0885b9636660ad
alex.zakarov <semerci394@gmail.com>
2023-12-08 15:09:48 +0300

    hotfix(root): rearrange signal struct

--- domains/neural_net/application/services/service.go
+++ domains/neural_net/application/services/service.go
@@ -398,7 +398,7 @@ func CalculateMaxPercentageDiffIndexes(data []float64, percentage float64, maxI 
 
 			}
 
-			if isDone && isDoneCount == 2 {
+			if isDone && isDoneCount == 3 {
 				signalPoints[buyPos] = 1
 				signalPoints[sellPos] = -1
 				isDone = false
